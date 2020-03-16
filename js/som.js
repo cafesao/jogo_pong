b7e7aca15d80c6833786b5d0f59cdbaac82cf8cf
cafesao <gabriel.diasdutra2@gmail.com>
2020-03-16 16:12:31 -0300

    Adicionando JS Sons

---
+++ js/som.js
@@ -0,0 +1,19 @@
+class sound {
+  constructor(src) {
+    this.sound = document.createElement('audio')
+    this.sound.src = src
+    this.sound.setAttribute('preload', 'auto')
+    this.sound.setAttribute('controls', 'none')
+    this.sound.style.display = 'none'
+    document.body.appendChild(this.sound)
+    this.play = () => {
+      this.sound.play()
+    }
+    this.stop = () => {
+      this.sound.pause()
+    }
+  }
+}
+
+export const somRaquete = new sound('../sound/raquete.mp3')
+export const somPonto = new sound('../sound/ponto.mp3')
